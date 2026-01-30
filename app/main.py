e1d62c9ef2544e62b864a2c2b69e5ce1d573febd
Kamil Cessak <kamil.cessak@mindconsulting.pl>
2026-01-30 13:23:49 +0100

    added possibility to exchange rates

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from app.database import init_db
 from app.routers import auth
 from app.routers import wallet
 from app.routers import rates
+from app.routers import exchange
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -16,6 +17,7 @@ app = FastAPI(title="Kantor Walut API", lifespan=lifespan)
 app.include_router(auth.router)
 app.include_router(wallet.router)
 app.include_router(rates.router)
+app.include_router(exchange.router)
 
 @app.get("/")
 async def root():
--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -2,3 +2,4 @@ from .user import UserRegister, UserResponse, UserLogin
 from .token import Token
 from .wallet import TopUpRequest, BalanceResponse
 from .rate import RateResponse
+from .exchange import ExchangeRequest, ExchangeResponse
